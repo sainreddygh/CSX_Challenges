83ed1a2d964de097f2ce2d590920ad156971b393
sainreddygh <nsainath.bits@gmail.com>
2019-01-05 12:06:18 -0800

    Creating a tree form int

---
+++ DpInJava/src/Challenges/TreeNodePlugins.java
@@ -0,0 +1,7 @@
+package Challenges;
+
+public class TreeNodePlugins {
+	public static TreeNode  listToTree(int[] list) {
+		return new TreeNode(4);
+	}
+}
--- DpInJava/src/main/Execution.java
+++ DpInJava/src/main/Execution.java
@@ -4,7 +4,8 @@ import Challenges.*;
 
 public class Execution {
 	public static void main(String[] args) {
-		System.out.println(LowestCA.get100());
+		TreeNode tree = TreeNodePlugins.listToTree(new int[] {2,4,5});
+		System.out.println(tree.val);
 	}
 
 }
