d15b75db63f3dc358b01b6a10ebd70dd70622405
sainreddygh <nsainath.bits@gmail.com>
2019-01-05 15:10:33 -0800

    binary tree traversals

--- DpInJava/src/Challenges/TreeNodePlugins.java
+++ DpInJava/src/Challenges/TreeNodePlugins.java
@@ -1,7 +1,41 @@
 package Challenges;
 
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class TreeNodePlugins {
-	public static TreeNode  listToTree(int[] list) {
-		return new TreeNode(4);
+	public static TreeNode  listToTree(String[] list) {
+		Queue<TreeNode> q = new LinkedList<>();
+		if(list[0] =="null") return null;
+		TreeNode result = new TreeNode(Integer.valueOf(list[0]));
+		q.add(result);
+		boolean leftDone = false;
+		for(int i=1; i<list.length; i++) {
+			if(!leftDone) {
+				if(list[i].equals("null")) {
+					q.peek().left = null;
+					System.out.println("null");
+				}else {
+					TreeNode tr =  new TreeNode(Integer.valueOf(list[i]));
+					q.peek().left =tr;			
+					q.add(tr);
+					System.out.println(tr.val);
+				}				
+				leftDone = true;
+			}else {
+				if(list[i].equals("null")) {
+					q.poll().right = null;
+					System.out.println("null");
+				}else {
+					TreeNode current = new TreeNode(Integer.valueOf(list[i]));
+					q.poll().right = current;
+					q.add(current);
+					System.out.println(current.val);
+				}				
+				leftDone = false;
+			}
+		}
+		return result;		
 	}
 }
--- DpInJava/src/main/Execution.java
+++ DpInJava/src/main/Execution.java
@@ -1,11 +1,36 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 import Challenges.*;
 
 public class Execution {
 	public static void main(String[] args) {
-		TreeNode tree = TreeNodePlugins.listToTree(new int[] {2,4,5});
-		System.out.println(tree.val);
+		TreeNode tree = TreeNodePlugins.listToTree(new String[] {"3","5","1","6","2","0","8","null","null","7","4"});
+		Queue<TreeNode> q =  new LinkedList<TreeNode>();
+		q.add(tree);
+		int current = 1;
+		while(!q.isEmpty()) {
+			int newLength = current;
+			current = 0;
+			System.out.println("");
+			for(int i = 0; i<newLength; i++) {
+				System.out.print(""+q.peek().val+", ");
+				if(q.peek().left!=null) {
+					q.add(q.peek().left);
+					current++;
+				}
+				if(q.peek().right!=null) {
+					q.add(q.peek().right);
+					current++;					
+				}
+				q.poll();
+			}
+		}
 	}
 
 }
