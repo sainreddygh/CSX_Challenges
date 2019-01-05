67315f304dc9d1c3856859a5b8b6f6b1a41d9521
sainreddygh <nsainath.bits@gmail.com>
2019-01-05 11:52:37 -0800

    initial commit

---
+++ DpInJava/src/Challenges/LowestCA.java
@@ -0,0 +1,7 @@
+package Challenges;
+
+public class LowestCA {
+	public static int get100() {
+		return 100;
+	}
+}
---
+++ DpInJava/src/main/Execution.java
@@ -0,0 +1,10 @@
+package main;
+
+import Challenges.*;
+
+public class Execution {
+	public static void main(String[] args) {
+		System.out.println(LowestCA.get100());
+	}
+
+}
