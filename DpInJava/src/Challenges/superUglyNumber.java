58fef567678f77e2216fa172381c15bd43d95515
sainreddygh <nsainath.bits@gmail.com>
2019-01-07 13:39:35 -0800

    Adding SuperUglyNumber code

---
+++ DpInJava/src/Challenges/superUglyNumber.java
@@ -0,0 +1,31 @@
+package Challenges;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class superUglyNumber {
+	public static int nthNumber(int n, int[] primes) {
+		int[] dp = new int[primes.length];
+        List<Integer> ans = new ArrayList<Integer>();
+        ans.add(1);
+        
+        while(ans.size()<n){
+            int lastVal = ans.get(ans.size()-1);
+            int currentMin = lastVal;
+            int currentIndex = -1;
+            for(int i =0; i<primes.length; i++){
+                if(primes[i]*ans.get(dp[i])>lastVal && (currentIndex == -1 || primes[i]*ans.get(dp[i]) <currentMin)){
+                    currentMin = primes[i]*ans.get(dp[i]);
+                    dp[i]++;
+                    if(currentIndex!=-1){
+                        dp[currentIndex]--;
+                    }
+                    currentIndex = i;
+                }
+            }
+            ans.add(currentMin);
+        }
+        return ans.get(ans.size()-1);
+	}
+
+}
