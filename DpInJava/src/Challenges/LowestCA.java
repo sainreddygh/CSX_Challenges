7bf3dbc738e4bbcc88478afdfc833159b65a2f74
sainreddygh <nsainath.bits@gmail.com>
2019-01-07 12:24:49 -0800

    Adding LCA node code

--- DpInJava/src/Challenges/LowestCA.java
+++ DpInJava/src/Challenges/LowestCA.java
@@ -1,7 +1,34 @@
 package Challenges;
 
+import java.util.Stack;
+
 public class LowestCA {
-	public static int get100() {
-		return 100;
+	public static TreeNode getLCA(TreeNode root, TreeNode p, TreeNode q) {
+		int additionalPushes = 0;
+        TreeNode anc = root;
+        Stack<TreeNode> s =  new Stack<TreeNode>();
+        TreeNode curr = root;
+        boolean foundOne = false;
+        while(curr!=null || !s.isEmpty()){
+            while(curr!=null){
+                s.push(curr);
+                if(foundOne) additionalPushes++;
+                curr=curr.left;
+            }
+            curr=s.pop();
+            if(foundOne){                
+                additionalPushes--;
+                if(additionalPushes < 0) {
+                    anc = curr;
+                    additionalPushes = 0;
+                }
+                if(curr == p || curr == q){ return anc;}
+            }else if(curr == p || curr == q) {
+                anc = curr;
+                foundOne =true;
+            }
+            curr = curr.right;
+        }
+        return anc;
 	}
 }
--- DpInJava/src/main/Execution.java
+++ DpInJava/src/main/Execution.java
@@ -11,7 +11,7 @@ import Challenges.*;
 public class Execution {
 	public static void main(String[] args) {
 		TreeNode tree = TreeNodePlugins.listToTree(new String[] {"3","5","1","6","2","0","8","null","null","7","4"});
-		LevelOrderTree.printLevelOrder(tree);
+		LowestCA.getLCA(tree);
 	}
 
 }
