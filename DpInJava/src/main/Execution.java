2eaaec8c9c2784a23fcff08fb09b703fd0a257bf
sainreddygh <nsainath.bits@gmail.com>
2019-01-08 12:52:12 -0800

    Adding singleNumberIII code

--- DpInJava/src/main/Execution.java
+++ DpInJava/src/main/Execution.java
@@ -10,7 +10,9 @@ import Challenges.*;
 
 public class Execution {
 	public static void main(String[] args) {
-		int[] input = new int[] {3,5,7,11,19,23,29,41,43,47};
-		System.out.println(superUglyNumber.nthNumber(15, input));
+		int[] result = SingleNumberIII.get2SingleNumbers(new int[] {1,2,1,3,2,5});
+		for(int n : result) {
+			System.out.println(n);
+		}
 	}
 }
