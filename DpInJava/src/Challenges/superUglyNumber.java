751195b7126d0a529c6e52b45ae14e6a8e27a34b
sainreddygh <nsainath.bits@gmail.com>
2019-01-10 08:46:36 -0800

    Testing SuperUglyNumber code

--- DpInJava/src/Challenges/superUglyNumber.java
+++ DpInJava/src/Challenges/superUglyNumber.java
@@ -16,6 +16,7 @@ public class superUglyNumber {
             	int currentValue = primes[i]*ans.get(dp[i]);
             	if(lastVal >= currentValue) {
             		dp[i]++;
+            		currentValue = primes[i]*ans.get(dp[i]);
             	}
             	if(currentIndex != -1 && currentMin<primes[i]) {
             		break;
@@ -29,35 +30,7 @@ public class superUglyNumber {
                     currentIndex = i;
                 }
             }
-            ans.add(currentMin);
-            
-//            int[] primeLevels  = new int[3];
-//            int[] primes = new int[]{2,3,5};
-//            List<Integer> ans =  new ArrayList<Integer>();
-//            ans.add(1);
-//            while(ans.size()<n){
-//                int lastVal = ans.get(ans.size()-1);
-//                int indexToIncrease = -1;
-//                int currentMin = 0;
-//                for(int i = 0; i<primes.length; i++){
-//                    int currentVal = primes[i]*ans.get(primeLevels[i]);
-//                    if(currentVal<=lastVal) primeLevels[i]++;
-//                    if(currentVal>lastVal && (indexToIncrease==-1 || currentVal<currentMin)){
-//                        currentMin = currentVal;
-//                        primeLevels[i]++;
-//                        if(indexToIncrease!=-1){
-//                            primeLevels[indexToIncrease]--;
-//                        }
-//                        indexToIncrease = i;
-//                        // if(indexToIncrease!=-1) primeLevels[indexToIncrease]--;
-//                        // primeLevels[i]++;
-//                        // indexToIncrease = i;                    
-//                        // currentMin = currentVal;
-//                    }
-//                }
-//                ans.add(currentMin);
-//            }
-//            return ans.get(ans.size()-1);   
+            ans.add(currentMin); 
         }
         System.out.println(ans);
         return ans.get(ans.size()-1);
