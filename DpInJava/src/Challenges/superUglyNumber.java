0dbbbc79e930fb937894d269bda40f4028ffb363
sainreddygh <nsainath.bits@gmail.com>
2019-01-10 08:44:34 -0800

    Fixing SuperUglyNumber code

--- DpInJava/src/Challenges/superUglyNumber.java
+++ DpInJava/src/Challenges/superUglyNumber.java
@@ -30,7 +30,36 @@ public class superUglyNumber {
                 }
             }
             ans.add(currentMin);
+            
+//            int[] primeLevels  = new int[3];
+//            int[] primes = new int[]{2,3,5};
+//            List<Integer> ans =  new ArrayList<Integer>();
+//            ans.add(1);
+//            while(ans.size()<n){
+//                int lastVal = ans.get(ans.size()-1);
+//                int indexToIncrease = -1;
+//                int currentMin = 0;
+//                for(int i = 0; i<primes.length; i++){
+//                    int currentVal = primes[i]*ans.get(primeLevels[i]);
+//                    if(currentVal<=lastVal) primeLevels[i]++;
+//                    if(currentVal>lastVal && (indexToIncrease==-1 || currentVal<currentMin)){
+//                        currentMin = currentVal;
+//                        primeLevels[i]++;
+//                        if(indexToIncrease!=-1){
+//                            primeLevels[indexToIncrease]--;
+//                        }
+//                        indexToIncrease = i;
+//                        // if(indexToIncrease!=-1) primeLevels[indexToIncrease]--;
+//                        // primeLevels[i]++;
+//                        // indexToIncrease = i;                    
+//                        // currentMin = currentVal;
+//                    }
+//                }
+//                ans.add(currentMin);
+//            }
+//            return ans.get(ans.size()-1);   
         }
+        System.out.println(ans);
         return ans.get(ans.size()-1);
 	}
 }
--- DpInJava/src/main/Execution.java
+++ DpInJava/src/main/Execution.java
@@ -10,8 +10,56 @@ import Challenges.*;
 
 public class Execution {
 	public static void main(String[] args) {
-		String a = "1+1i";
-		
-		System.out.println(a.split("\\+")[1]);
+		superUglyNumber.nthNumber(37, primes)
 	}
 }
+
+int[] dp = new int[primes.length];
+List<Integer> ans = new ArrayList<Integer>();
+ans.add(1);        
+while(ans.size()<n){
+    int lastVal = ans.get(ans.size()-1);
+    int currentMin = lastVal;
+    int currentIndex = -1;
+    for(int i =0; i<primes.length; i++){
+    	if(lastVal >= primes[i]*ans.get(dp[i])) {
+    		dp[i]++;
+    	}
+        if(primes[i]*ans.get(dp[i])>lastVal && (currentIndex == -1 || primes[i]*ans.get(dp[i]) <currentMin)){
+        	currentMin = primes[i]*ans.get(dp[i]);
+            dp[i]++;
+            if(currentIndex!=-1){
+                dp[currentIndex]--;
+            }
+            currentIndex = i;
+        }
+    }
+    ans.add(currentMin);
+}
+return ans.get(ans.size()-1);
+
+
+int[] dp = new int[primes.length];
+List<Integer> ans = new ArrayList<Integer>();
+ans.add(1);        
+while(ans.size()<n){
+    int lastVal = ans.get(ans.size()-1);
+    int currentMin = lastVal;
+    int currentIndex = -1;
+    for(int i =0; i<primes.length; i++){
+    	int currentValue = primes[i]*ans.get(dp[i]);
+    	if(lastVal >= currentValue) {
+    		dp[i]++;
+    	}
+        if(currentValue>lastVal && (currentIndex == -1 || currentValue <currentMin)){
+        	currentMin = currentValue;
+            dp[i]++;
+            if(currentIndex!=-1){
+                dp[currentIndex]--;
+            }
+            currentIndex = i;
+        }
+    }
+    ans.add(currentMin);
+}
+return ans.get(ans.size()-1);
