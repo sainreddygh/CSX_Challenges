b8879fa9c9dc2132ba71be15186d1e108a534226
sainreddygh <nsainath.bits@gmail.com>
2019-01-14 19:27:29 -0800

    Design patterns

--- DpInJava/src/Challenges/SuperUglyNumber2.java
+++ DpInJava/src/Challenges/SuperUglyNumber2.java
@@ -7,19 +7,6 @@ import java.util.PriorityQueue;
 
 public class SuperUglyNumber2 {
 	public static int nthSuperUglyumber(int n, int[] primes) {
-//		PriorityQueue<Num> pq =  new PriorityQueue<>();
-//		pq.add(new Num(10));
-//		System.out.println(pq.peek().val);
-//		pq.add(new Num(1));
-//		System.out.println(pq.peek().val);
-//		pq.add(new Num(20));
-//		System.out.println(pq.peek().val);
-//		pq.poll();
-//		System.out.println(pq.peek().val);
-//		pq.add(new Num(11));
-//		System.out.println(pq.peek().val);
-//		pq.poll();
-//		System.out.println(pq.peek().val);
 		
 		Num[] nums = new Num[] {new Num(3), new Num(9), new Num(4), new Num(6), new Num(2) };
 		for(Num m : nums) System.out.println(m.val);
