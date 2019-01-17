f3b2066e4f610dc44bb7f1361a06bf619df60013
sainreddygh <nsainath.bits@gmail.com>
2019-01-17 11:54:36 -0800

    Adding first n locations by distnce code

--- DpInJava/src/main/Execution.java
+++ DpInJava/src/main/Execution.java
@@ -21,6 +21,27 @@ public class Execution {
 		int c = (int) (Math.pow(a, 2) + Math.pow(b, 2));
 		System.out.println(c);
 	}
+	List<List<Integer>> getMiniDistancePoints(int totalLocations, List<List<Integer>> locations, int numDliveries) {
+		List<List<Integer>> ans =  new ArrayList<>();
+		PriorityQueue<Location> pq = new PriorityQueue<Location>((a,b)->distance(b)-distance(a));
+		for(List<Integer> l : locations) {
+			Location loc = new Location(l);
+			pq.add(loc);
+		}
+		for(int i=0; i<numDliveries; i++) {
+			ans.add(pq.poll().location);
+		}
+		return ans;
+	}
+	int distance(Location l) {
+		return l.location.get(0) * l.location.get(0) + l.location.get(1)* l.location.get(1);
+	}
+	class Location{
+		List<Integer> location;
+		public Location(List<Integer> location) {
+			this.location = location;
+		}		
+	}
 }
 
 
