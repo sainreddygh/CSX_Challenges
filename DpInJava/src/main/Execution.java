8eeb7d7b2a6ef75ff1f5b3a705c4de598cbb3f72
sainreddygh <nsainath.bits@gmail.com>
2019-01-24 10:54:11 -0800

    Design pattern changes

--- DpInJava/src/main/Execution.java
+++ DpInJava/src/main/Execution.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Observer;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.TreeMap;
 
 import Challenges.*;
 import ObserverDP.StockObserver;
@@ -17,7 +18,13 @@ import StrateDesignPattern.Dog;
 
 public class Execution {
 	public static void main(String[] args) {
-		List<int[]> ans =  new ArrayList<int[]>();
+		TreeMap<Integer, Integer> map =  new TreeMap<>();
+		map.put(2, 1291);
+		map.put(6, 1397932);
+		map.put(20, 1949832);
+		map.put(10, 13829);
+		System.out.println(map.ceilingEntry(15).getValue());
+		
 	}
 }
 
