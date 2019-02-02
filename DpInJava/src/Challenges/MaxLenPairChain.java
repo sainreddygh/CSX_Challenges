1a18783d70ce043b54cee3e9e296f9bea06ab416
sainreddygh <nsainath.bits@gmail.com>
2019-02-02 13:25:48 -0800

    Design patterns

--- DpInJava/src/Challenges/MaxLenPairChain.java
+++ DpInJava/src/Challenges/MaxLenPairChain.java
@@ -1,11 +1,21 @@
 package Challenges;
 
+import java.util.Arrays;
+
 public class MaxLenPairChain {
 	public static int getLongestChainLength(int[][] pairs) {
-		
-		
-		
-		return 10;
+		int n = pairs.length;
+		if(n<2) return n;
+		Arrays.sort(pairs, (a, b)->{return b[1]- a[1];});
+		int ans = 1;
+		int[] lastPair = pairs[0];
+		for(int i=1; i<n; i++) {
+			if(pairs[i][0]>lastPair[1]) {
+				ans++;
+				lastPair = pairs[i];
+			}
+		}		
+		return ans;
 	}
 
 }
