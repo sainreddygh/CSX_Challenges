b304a0ec13d9a48d54f3a1adc0a9d622d33c8c93
sainreddygh <nsainath.bits@gmail.com>
2019-02-02 18:38:43 -0800

    Design patterns

--- DpInJava/src/main/Execution.java
+++ DpInJava/src/main/Execution.java
@@ -19,7 +19,7 @@ import StrateDesignPattern.Dog;
 
 public class Execution {
 	public static void main(String[] args) {
-		String s = "Sainath reddy";
+		String s = "   Sainath reddy   ";
 		String[] sArray =  s.split(" ");
 		for(String sr : sArray)
 		System.out.println(sr);
