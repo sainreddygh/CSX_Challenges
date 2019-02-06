abe21947cb88606aa14c1639c77fbb19d8723947
sainreddygh <nsainath.bits@gmail.com>
2019-02-05 20:27:06 -0800

    Design patterns

--- DpInJava/src/Challenges/TopKFrequentElements.java
+++ DpInJava/src/Challenges/TopKFrequentElements.java
@@ -4,11 +4,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class TopKFrequentElements {
 	public static List<Integer> topKFrequent(int[] nums, int k){
-		 Map<Integer, Integer> map =  new HashMap<Integer, Integer>();
-        List<Integer> ans = new ArrayList<>();
+		Map<Integer, Integer> map =  new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        PriorityQueue<Integer> q =  new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
+        for(Map.Entry<Integer, Integer> m:map.entrySet()){
+            q.add(m.getKey());
+        }
+        List<Integer> ans =  new ArrayList<>();
+        while(k>0){
+            ans.add(q.poll);
+        }
         return ans;
 	}
 
