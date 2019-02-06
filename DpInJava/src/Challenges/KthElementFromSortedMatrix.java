d82fdc4d83d9282c2e7242c6482a606f69ee78ed
sainreddygh <nsainath.bits@gmail.com>
2019-02-06 14:47:10 -0800

    Design patterns

--- DpInJava/src/Challenges/KthElementFromSortedMatrix.java
+++ DpInJava/src/Challenges/KthElementFromSortedMatrix.java
@@ -1,15 +1,28 @@
 package Challenges;
 
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class KthElementFromSortedMatrix {
-	public static int getKthsmallestElement(int[][] nums, int k) {
-		int ans = 0;
+	public int getKthsmallestElement(int[][] nums, int k) {
+		int ans =0;
+		PriorityQueue<tuple> heap = new PriorityQueue<tuple>();
+		for(int i=0; i<nums.length; i++) {
+			heap.offer(new tuple(i,0,nums[i][0]));
+		}
 		return ans;
 	}
 	
-	class tuple implements Comparator<tuple>{
-				
+	class tuple implements Comparable<tuple>{
+		int x,y,val;
+		tuple(int x, int y, int val){
+			this.x = x;
+			this.y = y;
+			this.val=val;
+		}
+		public int compareTo(tuple t){
+			return val-t.val;
+		}
 	}
 	
 
