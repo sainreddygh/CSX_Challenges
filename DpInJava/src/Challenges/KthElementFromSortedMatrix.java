51c9d7fb3823858150a6c7c77641031a8eb426c8
sainreddygh <nsainath.bits@gmail.com>
2019-02-06 14:34:29 -0800

    Design patterns

---
+++ DpInJava/src/Challenges/KthElementFromSortedMatrix.java
@@ -0,0 +1,16 @@
+package Challenges;
+
+import java.util.Comparator;
+
+public class KthElementFromSortedMatrix {
+	public static int getKthsmallestElement(int[][] nums, int k) {
+		int ans = 0;
+		return ans;
+	}
+	
+	class tuple implements Comparator<tuple>{
+				
+	}
+	
+
+}
