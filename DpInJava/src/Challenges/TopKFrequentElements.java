ef4eda9e33cff61af7401b2602585da85a6fca24
sainreddygh <nsainath.bits@gmail.com>
2019-02-05 20:10:35 -0800

    Design patterns

--- DpInJava/src/Challenges/TopKFrequentElements.java
+++ DpInJava/src/Challenges/TopKFrequentElements.java
@@ -1,5 +1,15 @@
 package Challenges;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TopKFrequentElements {
+	public static List<Integer> topKFrequent(int[] nums, int k){
+		 Map<Integer, Integer> map =  new HashMap<Integer, Integer>();
+        List<Integer> ans = new ArrayList<>();
+        return ans;
+	}
 
 }
