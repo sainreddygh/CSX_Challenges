bb90f3bc361e2eef118203035a65e5d2b4928fb9
sainreddygh <nsainath.bits@gmail.com>
2019-02-08 10:52:02 -0800

    Desing Challenges

---
+++ DpInJava/src/Challenges/CoinChange2.java
@@ -0,0 +1,14 @@
+package Challenges;
+
+public class CoinChange2 {
+	public int getCombinations(int amount, int[] coins) {
+		return helper(amount, coins, 0);
+		
+	}
+	public int helper(int amount, int[] coins, int start) {
+		if(amount == 0) return 1;
+		if(start>= coins.length) return 0;
+		return helper(amount - coins[start], coins, start) + helper(amount, coins, start+1);
+	}
+
+}
