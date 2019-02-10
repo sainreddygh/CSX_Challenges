0347196aa740849539a01f602e3c01d7782f04d7
sainreddygh <nsainath.bits@gmail.com>
2019-02-10 06:46:06 -0800

    Data structures

--- DpInJava/src/Challenges/AllPathsToTarget.java
+++ DpInJava/src/Challenges/AllPathsToTarget.java
@@ -1,5 +1,35 @@
 package Challenges;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AllPathsToTarget {
+
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        int n =  graph.length;
+        List<List<Integer>>[] dp =  new ArrayList[graph.length];
+        for(int i= n-2; i>=0; i--){
+            List<List<Integer>> allPaths = new ArrayList();
+            for(int j : graph[i]){
+                if(j==n-1) {
+                   List<Integer> finalPath =new ArrayList();
+                   finalPath.add(i);
+                   finalPath.add(n-1);
+                   allPaths.add(finalPath);
+                    continue;
+                }
+                for(List<Integer> l : dp[j]){
+                    List<Integer> currentPath = new ArrayList();
+                    currentPath.add(i);
+                    for(int li : l){
+                        currentPath.add(li);
+                    }
+                    allPaths.add(currentPath);
+                }
+            }
+            dp[i]= allPaths;
+        }
+        return dp[0];
+    }
 
 }
