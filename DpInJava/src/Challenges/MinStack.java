2ca260f26946abd210211e4285ac69cdd6a296a5
sainreddygh <nsainath.bits@gmail.com>
2019-02-13 20:06:00 -0800

    Data structures

--- DpInJava/src/Challenges/MinStack.java
+++ DpInJava/src/Challenges/MinStack.java
@@ -1,5 +1,38 @@
 package Challenges;
 
+import java.util.Stack;
+
 public class MinStack {
+    Stack<Integer> s;
+    Stack<Integer> minS;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        s = new Stack();
+        minS =  new Stack();
+    }
+    
+    public void push(int x) {
+        if(s.isEmpty()){
+            s.push(x);
+            minS.push(x);
+        }else{
+            if(x<=minS.peek()) minS.push(x);
+            s.push(x);            
+        }
+    }
+    
+    public void pop() {
+        if(s.peek() == minS.peek()) minS.pop();
+        s.pop();
+    }
+    
+    public int top() {
+        return s.peek();
+    }
+    
+    public int getMin() {
+        return minS.peek();        
+    }
 
 }
