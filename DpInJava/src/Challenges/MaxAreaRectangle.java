89151f0a756c31e9ea0a42adb7ee1c0be84d3a60
sainreddygh <nsainath.bits@gmail.com>
2019-02-15 12:54:15 -0800

    Data structures

--- DpInJava/src/Challenges/MaxAreaRectangle.java
+++ DpInJava/src/Challenges/MaxAreaRectangle.java
@@ -5,12 +5,29 @@ import java.util.Map;
 
 public class MaxAreaRectangle {
 	public int getMaxArea(int[] heights) {
-		Map<Integer, Integer> map = new HashMap();
+		int n = heights.length;
 		int ans = 0;
-		for(int i=0; i<heights.length; i++) {
-			
+		int[] leftP = new int[n];
+		int[] rightP = new int[n];
+		for(int i=0; i<n; i++) {
+			int p=i-1;
+			while(p>0 && heights[p]>=heights[i]) {
+				p=leftP[p];
+			}
+			leftP[i]=p;
 		}
-		return 0;
+		rightP[n-1]=-1;
+		for(int i=n-2; i>=0; i--) {
+			int p=i+1;
+			while(p<n-1 && heights[p]>=heights[i]) {
+				p=rightP[p];
+			}
+			rightP[i]=p;
+		}
+		for(int i=0; i<n; i++) {
+			ans = Math.max(ans, heights[i]*(i-leftP[i]+rightP[i]-i-1));
+		}
+		return ans;
 	}
 
 }
