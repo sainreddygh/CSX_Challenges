bd01d56d0e84adeb866b1c14e580ba1823d67116
sainreddygh <nsainath.bits@gmail.com>
2019-02-15 11:10:01 -0800

    Data structures

--- DpInJava/src/Challenges/MaxAreaRectangle.java
+++ DpInJava/src/Challenges/MaxAreaRectangle.java
@@ -1,11 +1,15 @@
 package Challenges;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MaxAreaRectangle {
 	public int getMaxArea(int[] heights) {
 		Map<Integer, Integer> map = new HashMap();
-		
-		
-		
+		int ans = 0;
+		for(int i=0; i<heights.length; i++) {
+			
+		}
 		return 0;
 	}
 
