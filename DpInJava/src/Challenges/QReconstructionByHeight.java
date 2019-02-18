c498ccb78bce8640f96ce805833bd1608ee153c3
sainreddygh <nsainath.bits@gmail.com>
2019-02-17 20:59:18 -0800

    Data structures

---
+++ DpInJava/src/Challenges/QReconstructionByHeight.java
@@ -0,0 +1,31 @@
+package Challenges;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class QReconstructionByHeight {
+	public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b)->{
+            if(a[0]<b[0]) return 1;
+            if(a[0]>b[0]) return -1;
+            if(a[0]==b[0] && a[1]<b[1]) return -1;
+            else return 1;
+        });
+        List<int[]> list = new ArrayList();
+        for(int i=0; i<people.length; i++) {
+        	list.add(people[i][1], people[i]);
+        }
+        int[][] ans =  new list.toArray();
+        return ans;
+        
+        
+    }
+    class Point{
+        int x,y;
+        Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
