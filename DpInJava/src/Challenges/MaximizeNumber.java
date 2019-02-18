c70a5f7e84dfe13175d268a2f9800a521524ac13
sainreddygh <nsainath.bits@gmail.com>
2019-02-18 12:13:09 -0800

    Algorithms

--- DpInJava/src/Challenges/MaximizeNumber.java
+++ DpInJava/src/Challenges/MaximizeNumber.java
@@ -1,7 +1,32 @@
 package Challenges;
 
+import java.util.Stack;
+
 public class MaximizeNumber {
 	public int maximumSwap(int num) {
-        
+		int[] values = new int[9];
+		int i=0;
+		while(i<9 && num%10!=0) {
+			values[i++]=num%10;
+			num/=10;
+		}
+		int minIndex = i;
+		while(minIndex>0 && values[minIndex]>values[minIndex-1])minIndex--;
+		if(minIndex==0) return num;
+		int maxIndex = minIndex-1;
+		for(int j = maxIndex; j>=0; j--) {
+			if(values[j]>values[maxIndex]) {
+				while(minIndex<i && values[minIndex+1]<values[j])minIndex++;
+				maxIndex=j;
+			}
+		}
+		int t = values[minIndex];
+		values[minIndex] = values[maxIndex];
+		values[maxIndex] = t;
+		int ans=1;
+		while(i>=0) {
+			ans=ans*10+values[i--];
+		}
+		return ans;
     }
 }
