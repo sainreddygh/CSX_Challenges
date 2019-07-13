168cca3514b4cdca959921454e1a2e33159c9636
sainreddygh <nsainath.bits@gmail.com>
2019-07-12 17:38:05 -0700

    testing changes

--- DpInJava/FE/test1/main.js
+++ DpInJava/FE/test1/main.js
@@ -1,3 +1,8 @@
 function switchTab(e){
-    alert(e);
+    let mainDiv = document.getElementsByClassName("main-div")[0];
+    let childNodes = mainDiv.getElementsByTagName("a");  
+    for(let i = 0; i<childNodes.length; i++){
+        childNodes[i].className = "";
+    }
+    e.currentTarget.className+=" active";
 }
